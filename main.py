aafcd74992c6e0b576c92a09f918092c1a87d0ad
htosy <den2283283@gmail.com>
2024-07-03 17:26:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLay
 
 app = QApplication([])
 main_win = QWidget()
+main_win.resize(400,400)
+main_win.setWindowTitle('Визначник переможця')
+button = QPushButton('Згенерувати')
+text = QLabel('Натисни, щоб дізнатися переможця')
+winner = QLabel('?')
+
+line = QVBoxLayout()
+line.addWidget(text, alignment = Qt.AlignCenter)
+line.addWidget(winner, alignment = Qt.AlignCenter)
+line.addWidget(button, alignment = Qt.AlignCenter)
+main_win.setLayout(line)
 main_win.show()
 app.exec_()
